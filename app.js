c7fb255a8d89966824c94a1f0397c1f8554a3d27
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-22 10:42:50 -0500

    made routeController.js

--- app.js
+++ app.js
@@ -24,9 +24,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( methodOverride('_method'));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/controllers', routeController)
+
+
+var db = mongoose.connection
+mongoose.connect('mongodb://localhost/API-practice')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
