7be4da564c5fe9b49c91e4a960ed7e4ec1490b10
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-22 11:06:17 -0500

    added Schemas

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,2 +1,29 @@
 var express = require('express')
 var router = express.Router()
+
+
+///index route
+router.get('/', (req, res) => {
+    res.send("welcome to my page")
+})
+
+/// show route
+router.get('/:id', (req, res) => {
+    var id = req.params.id,
+})
+///create route
+router.post('/', (req, res) => {
+    var info = req.body
+})
+///update route
+router.put('/:id', (req, res) => {
+
+})
+
+//user delete
+router.delete('/:id', (req, res) => {
+
+})
+
+
+module.exports = router
---
+++ db/schema.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+mongoose.Promise = global.Promise
+
+
+///created schema for items
+var ItemSchema = new Schema({
+    name: String
+})
+///for user
+var UserSchema = new Schema({
+    first_name: String,
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        created_at: date,
+        updated_at: date,
+        items: [ ItemSchema ]
+    }
+})
+
+UserSchema.pre('save', (next) => {
+    var now = new Date()
+    This.updated_at = now
+    if(!this.created_at) {
+        this.created_at = now
+    }
+    next()
+})
+
+
+module.exports = {
+    UserSchema,
+    ItemSchema
+}
